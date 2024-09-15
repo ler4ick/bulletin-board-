a22410099a4977ddaf70e38f422cbe4f2edd8ce0
ler4ick <skorlera516@gmail.com>
2024-09-15 22:24:23 +0300

    верстка поиска

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
