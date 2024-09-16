70d9d0d5014116ea983fa0ea43a8261f89d185e2
ler4ick <skorlera516@gmail.com>
2024-09-16 15:06:10 +0300

    сверстала карточку товара

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
+import { AdComponent } from './pages/ad/ad.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, SearchComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    AdComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
